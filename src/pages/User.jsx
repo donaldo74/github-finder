5e706ea6cc232661fe0ee2cd57baf860a48e7bff
Gavin Coleman <gavinjcoleman@gmail.com>
2022-12-08 19:57:41 +0000

    Repository list not displaying yet

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -2,16 +2,19 @@ import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa';
 import { useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Spinner from '../components/layout/Spinner';
+import RepoList from '../components/repos/RepoList';
 import { useParams } from 'react-router-dom';
 
 import GithubContext from '../context/github/GithubContext';
 
 function User() {
-	const { getUser, user, loading } = useContext(GithubContext);
+	const { getUser, user, loading, getUserRepos, repos } =
+		useContext(GithubContext);
 
 	const params = useParams();
 	useEffect(() => {
 		getUser(params.login);
+		getUserRepos(params.login);
 	}, []);
 
 	const {
@@ -157,6 +160,8 @@ function User() {
 						</div>
 					</div>
 				</div>
+
+				<RepoList repos={repos} />
 			</div>
 		</>
 	);
